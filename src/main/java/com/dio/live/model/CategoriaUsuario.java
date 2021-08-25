89080169224843cce76aa7b00bf1b2601d510c7d
Saulo Costa <saulomlcosta10@gmail.com>
2021-08-24 23:51:17 -0300

    Model created and add some tables

---
+++ src/main/java/com/dio/live/model/CategoriaUsuario.java
@@ -0,0 +1,14 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class CategoriaUsuario {
+    private long id;
+    private String descricao;
+}
